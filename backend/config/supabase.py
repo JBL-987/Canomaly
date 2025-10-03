add0b4b688e257724da63e031c2d5aa071ddc2d7
JBL-987 <jasonbrandonloi03@gmail.com>
2025-10-03 20:55:36 +0700

    removed the app

--- backend/config/supabase.py
+++ backend/config/supabase.py
@@ -2,8 +2,7 @@ from supabase import create_client
 import os
 from dotenv import load_dotenv
 
-
-load_dotenv('.env.local')
+load_dotenv()
 
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
